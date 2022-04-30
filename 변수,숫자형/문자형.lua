feb2fe63ef0998b93cee0036e6822c85eb6c3777
AppiLia <thegrenadierguards@gmail.com>
2022-04-30 17:30:27 +0900

    문자형

---
+++ 변수,숫자형/문자형.lua
@@ -0,0 +1,43 @@
+print('He said "Hello."')
+print("He said \"Hello.\"")
+print("I'm fine")
+print('I\'m fine')
+
+local s = 'hello world'
+print(s)
+local x ="Hi lua"
+print(x)
+local str = 'Hi\nyou'
+print(str)
+local str = 'one\ttow\tthree'
+print(str)
+
+print('1\t2\t3')
+print('one\ttwo\tthree')
+print('uno\tdos\ttres')
+print('oneeeeeee\ttwoooooooooooooo\tthreeeeeeeee')
+
+print('this\that')
+print('this\\that')
+print("He said \"Hi\"")
+print('He said "Hi"')
+
+
+local s = 'Hi salesio'
+local len = #s
+print(#s)
+print(len)
+print(#s+10)
+
+local s1,s2 ='Hello ','world'
+local s3 = s1..s2
+local s4=s1..s2..s3
+print(s3)
+print(s4)
+
+local a,s =11.2,'Hello'
+local t = type(a)
+print(type(a))
+print(type(s))
+print(t)
+print(type(t))
