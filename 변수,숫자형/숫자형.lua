96bc71ec1a1f0096a944168c548283f1179e2d1e
AppiLia <thegrenadierguards@gmail.com>
2022-04-30 17:13:54 +0900

    숫자형

---
+++ 변수,숫자형/숫자형.lua
@@ -0,0 +1,24 @@
+local x,y =3,4
+x=x+y
+y=y-5
+local a=x*y
+local b=x/y
+local c=3/5
+print(a,b,c)
+
+local m =3
+m=10-m
+local n=-m
+print(m,n)
+
+local r,t,i,u =3%2, 14%3, 2.3%2, 2.3%1.1
+print(r,t,i,u)
+
+local a = 2^10
+print(a)
+a = 2^0.5
+print(a)
+a=0.3^1.5
+print(a)
+a=2^-1
+print(a)
