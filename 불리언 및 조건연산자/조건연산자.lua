2e4aecfabd92b0525d52819395063901a8983b87
AppiLia <thegrenadierguards@gmail.com>
2022-05-02 11:35:29 +0900

    조건연산자

---
+++ 불리언 및 조건연산자/조건연산자.lua
@@ -0,0 +1,19 @@
+local a,b,c =1,2,1
+print(a==b)
+
+local d = a~=b
+print(d)
+
+local e = a<b
+local f = a==c
+print(e,f)
+
+local g ='hi'
+local f = a == g
+print(f)
+
+local s1, s2 = 'hi','hello'
+local s3 = 'hi'
+print(s1~=s2)
+print(s1 == s3)
+
