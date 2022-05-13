d5d6ee838310ea1fef20ca15038cc85f04a73f50
AppiLia <thegrenadierguards@gmail.com>
2022-05-13 23:09:47 +0900

    뭔가_에러가_난다

--- 함수/함수3.lua
+++ 함수/함수3.lua
@@ -8,5 +8,23 @@ local function show(t)
     print('}')
 end
 
-local a ={11,22}
+local a ={11,22,x=33}
 show(a)
+
+local function foo(t)
+    t.y=-10
+end
+foo(a)
+show(a)
+
+
+--가변개수인자
+--arg 라는 테이블에 인자들이 순서대로 저장이 되어있다. (자동생성)
+local function foo(...)
+    show(arg)
+end
+
+foo(11)
+foo(22,33)
+foo(1,2,3,4)
+
