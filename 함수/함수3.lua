b2c83cdb3e25b55f4362cfe77c21173c1f43fe9f
AppiLia <thegrenadierguards@gmail.com>
2022-05-14 15:30:35 +0900

    ...하고_arg.n이_잘_안됨

--- 함수/함수3.lua
+++ 함수/함수3.lua
@@ -8,23 +8,57 @@ local function show(t)
     print('}')
 end
 
-local a ={11,22,x=33}
-show(a)
+--local a ={11,22,x=33}
+--show(a)
 
-local function foo(t)
-    t.y=-10
-end
-foo(a)
-show(a)
+--local function foo(t)
+--    t.y=-10
+--end
+--foo(a)
+--show(a)
 
 
 --가변개수인자
 --arg 라는 테이블에 인자들이 순서대로 저장이 되어있다. (자동생성)
-local function foo(...)
-    show(arg)
+--local function foo(...)
+--    show(arg)
+--end
+
+--foo(11)
+--foo(22,33)
+--foo(1,2,3,4)
+
+--local function totalsum(...)
+--    local sum=0
+--    for k=1,arg.n do
+--        sum=sum+arg[k]
+--    end
+--    return sum
+--end
+
+--local a = totalsum(11,22)
+--print(a)
+--a=totalsum(11,22,51,67,10)
+--print(a)
+
+
+local function calc(s, ...)
+    local ret
+    if s=='add'then
+        ret=0
+        for k=1,arg.n do
+            ret=ret+arg[k]
+        end
+    elseif s=='mul' then
+        ret=1
+        for k=1,arg.n do
+            ret=ret*arg[k]
+        end
+    end
+    return ret
 end
 
-foo(11)
-foo(22,33)
-foo(1,2,3,4)
-
+local a = calc('add',2,3,4)
+print(a)
+local a = calc('mul',2,3,4,2)
+print(a)
