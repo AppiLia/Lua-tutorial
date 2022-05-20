036b4353d4867f8352199e0cf3dfe350b1414d18
AppiLia <thegrenadierguards@gmail.com>
2022-05-20 23:23:39 +0900

    math함수

---
+++ 함수/math모듈.lua
@@ -0,0 +1,14 @@
+--math 모듈
+print (math.abs(-10)) --절대값
+print(math.pi)
+print(math.sin(math.pi/2))
+print(math.max(10,-1,100,-1000))
+print(math.min(10,-1,100,-1000))
+
+math.randomseed(os.time()) --시간마다 랜덤시드 설정
+
+print(math.random(6))
+print(math.random(6))
+print(math.random(6))
+
+print(math.sqrt(2)) --루트
